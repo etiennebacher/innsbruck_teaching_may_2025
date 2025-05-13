de47883302f39ccbf244e5a0b24950bda759b512
etiennebacher <etienne.bacher@protonmail.com>
2025-05-13 22:02:48 +0200

    demo for part 1

--- webscraping_part_1/demo.R
+++ webscraping_part_1/demo.R
@@ -6,7 +6,7 @@
 
 library(tidyverse)
 library(rvest)
-library(janitor)
+library(polite)
 
 
 ### Part 1: get party results for Bacares in 2019
@@ -16,15 +16,8 @@ url <- "https://resultados.elpais.com/elecciones/2019/municipales/01/04/19.html"
 html <- read_html(url)
 
 html |> 
-  html_element("h1") |> 
-  html_text()
-
-html |> 
   html_element("#tablaVotosPartidos") |> 
-  html_table() |> 
-  clean_names()
-
-
+  html_table() 
 
 ### Part 1 bis: being polite
 
@@ -33,177 +26,222 @@ url <- bow("https://resultados.elpais.com/elecciones/2019/municipales/01/04/19.h
 html <- scrape(url)
 
 html |> 
-  html_element("h1") |> 
-  html_text()
-
-html |> 
   html_element("#tablaVotosPartidos") |> 
-  html_table() |> 
-  clean_names()
+  html_table() 
+
+# Use the argument "delay" to reduce the delay (default is 5 sec) but don't 
+# forget to also specify the "user_agent" argument in this case.
+url2 <- bow(url, user_agent = "teaching-webscraping", delay = 2)
 
 
-# Reduce the delay between scraping but also change the user agent so that
-# the package is not wrongly accused
+### Part 2: generalize this to all communities + provinces + municipalities
+###
+### To build all those combinations, we must first get all values for 
+### communities, then get all values for provinces (for each community), then
+### get all values for municipalities (for each province)
 
-# To be used in bow()
-user_agent <- "Teaching webscraping -- Etienne Bacher"
-set_scrape_delay(2)
+# ----------------------------------------
+# Get all comunidad IDs and names
 
-
-
-### Part 2: generalize this to all provinces + departments + municipalities
-
-# URL: first number is comunidad, second one is province, third one is
-# municipality
-
-# Problem: the values for province depend on the comunidad, and the values
-# for municipality depend on the province
-
-# Also: we want the name of the place + their id on the website
-
-# Get name and index for comunidad
-
-com_id <- c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11",
-            "12", "13", "14", "15", "16", "17", "18", "19")
-address <- bow(
-  "https://resultados.elpais.com/elecciones/2019/municipales/",
-  user_agent = user_agent
-)
-
-com_info <- address |> 
-  scrape() |>
-  html_element("#comboCA") |>
+# Get the selector for "Comunidad" and then select its "children", meaning
+# all the possible options in this selector
+choices <- scrape(url2) |> 
+  html_element("#comboCA") |> 
   html_children()
 
-com_info |>
+com_ids <- choices |> 
   html_attr("id")
 
-com_info |>
+com_names <- choices |> 
   html_text()
 
-com_name <- data.frame(
-  com_index = com_info |>
-    html_attr("id"),
-  comunidad = com_info |>
-    html_text()
-) |>
-  filter(comunidad != "Comunidad") |>
-  mutate(com_index = ifelse(nchar(com_index) == 1,
-                            paste0("0", com_index),
-                            com_index))
+# Clean the ids: 
+# - the first choice in the selector doesn't have any id (it's NA) so we drop 
+#   that
+# - the ids must always have two digits, so we add a trailing 0 for single-digit
+#   number 
+com_ids <- com_ids[!is.na(com_ids)]
+com_ids <- sapply(com_ids, function(x) {
+  if (nchar(x) == 1) {
+    paste0("0", x)
+  } else {
+    x
+  }
+})
+com_ids <- unname(com_ids)
+com_ids
+
+# Clean the names: 
+# - the first choice in the selector is "Comunidad", which is not a real choice
+#   so we drop that
+com_names <- com_names[com_names != "Comunidad"]
+com_names
+
+# Store the results for later
+com <- data.frame(id = ids, name = com_names)
 
 
-### Get name and index for province
+# ----------------------------------------
+# Get all province IDs and names
+# We have to loop through all possible "com_ids"
 
-prov_name <- list()
-for (i in com_id) {
+# Make a list that will store all the dataframes, one per community. At the
+# end of the loop, we'll bind them to have a single dataframe with all 
+# community-province combinations.
+provinces <- list()
+
+for (com_id in com_ids) {
+  message("Getting provinces for comunidad ", com_id)
   
-  message("Getting provinces for comunidad ", i)
+  url <- bow(paste0("https://resultados.elpais.com/elecciones/2019/municipales/", com_id))
   
-  # Get names
-  url_i <- nod(address, paste0("elecciones/2019/municipales/", i))
-  choices <- scrape(url_i) |>
-    html_element("#comboCIR") |>
+  ### More or less the same scraping and cleaning steps as before
+  
+  choices <- scrape(url) |> 
+    html_element("#comboCIR") |> 
     html_children()
   
-  names_prov <- html_text(choices)
-  id_prov <- html_attr(choices, "id")
-  id_prov <- id_prov[!is.na(id_prov)]
+  prov_ids <- choices |> 
+    html_attr("id")
   
-  # Remove placeholder
-  names_prov <- names_prov[names_prov != "Provincia"]
-  if (length(names_prov) == 0) {
-    names_prov <- "Single provincia"
-    id_prov <- NA
-  } 
-
-  prov_name[[paste0("id", i)]] <- data.frame(
-    comunidad = i,
-    prov_index = id_prov,
-    provincia = names_prov
-  )
+  prov_names <- choices |> 
+    html_text()
+  
+  prov_ids <- prov_ids[!is.na(prov_ids)]
+  prov_ids <- sapply(prov_ids, function(x) {
+    if (nchar(x) == 1) {
+      paste0("0", x)
+    } else {
+      x
+    }
+  })
+  prov_ids <- unname(prov_ids)
+  prov_ids
+  
+  prov_names <- prov_names[prov_names != "Provincia"]
+  prov_names
+  
+  # It's possible that a community doesn't have any province, in which case
+  # prov_names will be empty, so we only return a data.frame when it's not.
+  if (length(prov_names) > 0) {
+    provinces[[com_id]] <- data.frame(
+      com_id = com_id,
+      prov_id = prov_ids,
+      name = prov_names
+    )
+  }
 }
 
-prov_name <- data.table::rbindlist(prov_name)
+provinces <- bind_rows(provinces)
 
 
-### Merge index and names for comunidad and provincia
 
-com_prov_name <- left_join(
-  com_name,
-  prov_name,
-  by = c("com_index" = "comunidad")
-) |>
-  mutate(
-    provincia = ifelse(
-      comunidad %in% c("Melilla", "Ceuta"),
-      "Not provincia",
-      provincia
-    ),
-    prov_index = ifelse(
-      nchar(prov_index) == 1,
-      paste0("0", prov_index),
-      prov_index
-    )
-  )
+# ----------------------------------------
+# MISSING PART FROM THE LIVE DEMO
+# 
+# We have seen that some comunidad have no (or only one provincia), hence the
+# second selector was empty. We discarded those cases but we still to include
+# them manually in the list of combinations otherwise they won't be scraped.
+# 
+# I create this dataframe manually and bind it to the one we generated 
+# automatically.
 
-# Note : 50 comunidad (7 of which are made of a single province), 2 are not comunidad (Melilla and Ceuta)
+missing_combs <- data.frame(
+  com_id = c("03", "04", "06", "12", "15", "13", "16", "18", "19"),
+  prov_id = c("33", "07", "39", "28", "30", "31", "26", "51", "52"),
+  name = c("Asturias", "Baleares", "Cantabria", "Madrid", "Murcia", "Navarra", "La Rioja", "Ceuta", "Melilla")
+)
+
+provinces <- bind_rows(provinces, missing_combs)
 
 
-### We have info on the list of comunidad + province.
-### Now we can go on each page, get the list of municipalities.
+# ----------------------------------------
+# Get all municipality IDs and names
+# We have to loop through all possible combinations of community-province stored
+# in the "provinces" dataframe we created above.
 
-municip_name <- list()
-for (row in 1:nrow(com_prov_name)) {
-  com_id <- com_prov_name[row, "com_index"]
-  prov_id <- com_prov_name[row, "prov_index"]
+municipalities <- list()
+
+for (row in 1:nrow(provinces)) {
+  
+  com_id <- provinces[row, "com_id"]
+  prov_id <- provinces[row, "prov_id"]
+  
   message("Getting municipalities for comunidad ", com_id, " and province ", prov_id)
   
-  prov_url <- paste0("elecciones/2019/municipales/", com_id, "/", prov_id, ".html") |> 
-    nod()
-  municip_selector <- scrape(prov_url) |> 
-    html_element("#comboMUN") |>
-    html_children() 
+  url <- bow(paste0("https://resultados.elpais.com/elecciones/2019/municipales/", com_id, "/", prov_id, ".html"))
   
-  municip_ids <- html_attr(municip_selector, "id")
-  municip_names <- html_text(municip_selector)
+  choices <- scrape(url) |> 
+    html_element("#comboMUN") |> 
+    html_children()
   
-  municip_ids <- municip_ids[!is.na(municip_ids)]
-  municip_names <- municip_names[municip_names != "Municipio"]
+  mun_ids <- choices  |> 
+    html_attr("id")
   
-  municip_name[[paste0("id", i)]] <- data.frame(
-    com_index = com_id,
-    prov_index = prov_id,
-    municip_index = municip_ids,
-    municip = municip_names
-  )
+  mun_names <- choices |> 
+    html_text()
+  
+  mun_ids <- mun_ids[!is.na(mun_ids)]
+  mun_ids <- sapply(mun_ids, function(x) {
+    if (nchar(x) == 1) {
+      paste0("0", x)
+    } else {
+      x
+    }
+  })
+  mun_ids <- unname(mun_ids)
+  mun_ids
+  
+  mun_names <- mun_names[mun_names != "Municipio"]
+  mun_names
+  
+  if (length(mun_names) > 0) {
+    municipalities[[row]] <- data.frame(
+      com_id = com_id,
+      prov_id = prov_id,
+      mun_id = mun_ids,
+      mun_name = mun_names
+    )
+  }
 }
 
-municip_name
+municipalities <- bind_rows(municipalities)
 
 
+# --------------------------------------
+# Get table for each municipality
+# 
+# We now have all the combinations of community-province-municipality, so we
+# can build all URLs to scrape.
+# 
+# WARNING: this may take a while, you probably want to let it run 1min for the
+# demo
 
-# Some comunidad have no (or only one provincia), hence the second
-# selectinput is empty, so I need to provide the provincia index
-# manually.
-if (com_name == "Asturias") {
-  prov_id <- "33"
-} else if (com_name == "Baleares") {
-  prov_id <- "07"
-} else if (com_name == "Cantabria") {
-  prov_id <- "39"
-} else if (com_name == "Madrid") {
-  prov_id <- "28"
-} else if (com_name == "Murcia") {
-  prov_id <- "30"
-} else if (com_name == "Navarra") {
-  prov_id <- "31"
-} else if (com_name == "La Rioja") {
-  prov_id <- "26"
-} else if (com_name == "Ceuta") {
-  prov_id <- "51"
-} else if (com_name == "Melilla") {
-  prov_id <- "52"
+results <- list()
+
+for (row in 1:nrow(municipalities)) {
+  
+  com_id <- municipalities[row, "com_id"]
+  prov_id <- municipalities[row, "prov_id"]
+  mun_id <- municipalities[row, "mun_id"]
+  
+  message("Getting table for comunidad ", com_id, ", province ", prov_id, " and municipality ", mun_id)
+  
+  url <- bow(paste0("https://resultados.elpais.com/elecciones/2019/municipales/", com_id, "/", prov_id, "/", mun_id, ".html"))
+  
+  res <- scrape(url) |> 
+    html_element("#tablaVotosPartidos") |> 
+    html_table()
+  
+  res[["com_id"]] <- com_id
+  res[["prov_id"]] <- prov_id
+  res[["mun_id"]] <- mun_id
+  
+  if (length(res) > 0) {
+    results[[row]] <- res
+  }
 }
+
+bind_rows(results)
 
 
